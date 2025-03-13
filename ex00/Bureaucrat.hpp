80c6be5332305abe2422ef4e74d4fc2cb8fbc6eb
mahahahad <mahad.abd2003@gmail.com>
2025-03-13 16:41:49 +0400

    chore: add files

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#define RED "\033[31m"
+#define RESET "\033[0m"
+
+#include <iostream>
+#include <exception>
+#include <string>
+
+class   Bureaucrat {
+    public:
+        Bureaucrat( const std::string& name, const int grade );
+        ~Bureaucrat();
+        Bureaucrat( const Bureaucrat& copy );
+        Bureaucrat& operator=( const Bureaucrat& copy );
+        std::string getName( void ) const;
+        int         getGrade( void ) const;
+        void        increaseGrade( void );
+        void        decreaseGrade( void );
+
+    public:
+        class   GradeTooLowException: public std::exception {
+            public:
+                virtual const char* what();
+        };
+        class   GradeTooHighException: public std::exception {
+            public:
+                virtual const char* what();
+        };
+
+    private:
+        const std::string   name;
+        int                 grade;
+
+    private:
+        Bureaucrat();
+};
+
+std::ostream&   operator<<(std::ostream& output, const Bureaucrat& bc);
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,37 @@
+#include "Bureaucrat.hpp"
+
+int main(void) {
+    {
+        Bureaucrat john("John", 10);
+        Bureaucrat jane("Jane", 5);
+
+        std::cout << john << jane;
+        // Only grade changes because name cannot be changed
+        john = jane;
+        std::cout << john;
+    }
+    {
+        try {
+            Bureaucrat invalid("invalid", 2);
+            
+            std::cout << invalid;
+            invalid.increaseGrade();
+            std::cout << invalid;
+            invalid.increaseGrade();
+
+        } catch (Bureaucrat::GradeTooHighException &e) {
+            std::cout << e.what();
+        }
+    }
+    {
+        try {
+            Bureaucrat invalid2("invalid2", 149);
+            std::cout << invalid2;
+            invalid2.decreaseGrade();
+            std::cout << invalid2;
+            invalid2.decreaseGrade();
+        } catch (Bureaucrat::GradeTooLowException &e) {
+            std::cout << e.what();
+        }
+    }
+}
